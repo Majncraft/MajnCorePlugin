5a80b8ce02fc9a456fc5ff0750ea2890525fc7db
Welfare93 <frycjiri@gmail.com>
2014-09-11 18:46:41 +0200

    Deleting reflection, will be added in future

--- src/main/java/cz/majncraft/core/PluginLoader.java
+++ src/main/java/cz/majncraft/core/PluginLoader.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.reflections.Reflections;
 
 import cz.majncraft.MajnCorePlugin;
 import cz.majncraft.api.MajnPlugin;
+import cz.majncraft.plugins.LogCleaner;
 
 public class PluginLoader {
 
@@ -19,20 +19,7 @@ public class PluginLoader {
 	}
 	public static void loadPlugins()
 	{
-    	MajnCorePlugin.instance.getLogger().info("Starting lookup for majnplugins");
-    	Reflections reflections = new Reflections("cz.majncraft.plugins");    
-    	Set<Class<? extends MajnPlugin>> classes = reflections.getSubTypesOf(MajnPlugin.class);
-    	MajnCorePlugin.instance.getLogger().info("Founded "+classes.size()+" majnplugins");
-		for(Class<? extends MajnPlugin> cl:classes)
-			try {
-				plugins.add(cl.newInstance());
-			} catch (InstantiationException e) {
-		    	MajnCorePlugin.instance.getLogger().info(e.getLocalizedMessage());
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-		    	MajnCorePlugin.instance.getLogger().info(e.getLocalizedMessage());
-				e.printStackTrace();
-			}
+		plugins.add(new LogCleaner());
 	}
 	
 }
